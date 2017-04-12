c8d00cc3a0efdbaee6971050d076146e27f131f6
lindeyi <87617707@qq.com>
2017-04-12 10:02:59 +0800

    u

--- template/src/main.js
+++ template/src/main.js
@@ -2,12 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-{{#router}}
-import router from './router';
-{{/router}}
-{{#i18n}}
-import i18n from './i18n';
-{{/i18n}}
 import Mint from 'mint-ui';
 import 'mint-ui/lib/style.css';
 Vue.use(Mint);
@@ -17,12 +11,6 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#i18n}}
-  i18n,
-  {{/i18n}}
-  {{#router}}
-  router,
-  {{/router}}
   template: '<App/>',
   components: { App }
 });
