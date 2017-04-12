510b83768725cb43f6876ec9d323ccdc609eba37
lindeyi <87617707@qq.com>
2017-04-12 10:56:22 +0800

    u

--- meta.js
+++ meta.js
@@ -32,5 +32,5 @@ module.exports = {
   "filters": {
     "src/views/demo/*": "demo"
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "ºÃ°¡To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
